6acc8548c0c234f1aa5338adc0dd82862a39d619
anshulrouthu <anshul.m67@gmail.com>
2014-05-29 00:42:21 -0400

    Making changes to the setup environment compatible for Mac OSX and Linux

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-wget http://arduino.googlecode.com/files/arduino-1.0.5-linux32.tgz
-tar xvzf arduino-1.0.5-linux32.tgz
-rm arduino-1.0.5-linux32.tgz
+if [ "$(uname)" = "Darwin" ]; then
+    if [ ! -f "arduino-1.0.5-macosx.zip" ]; then
+        curl http://arduino.googlecode.com/files/arduino-1.0.5-macosx.zip -o arduino-1.0.5-macosx.zip
+    fi
+    unzip arduino-1.0.5-macosx.zip
+    mkdir arduino-1.0.5
+    cp -r Arduino.app/Contents/Resources/Java/* arduino-1.0.5
+    rm -rf Arduino.app
+    rm arduino-1.0.5-macosx.zip
 
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    if [ ! -f "arduino-1.0.5-linux32.tgz" ]; then
+        wget http://arduino.googlecode.com/files/arduino-1.0.5-linux32.tgz
+    fi
+    tar xvzf arduino-1.0.5-linux32.tgz
+    rm arduino-1.0.5-linux32.tgz
+fi
